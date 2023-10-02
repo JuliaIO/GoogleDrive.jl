8c8c20faa36577576a660febe025b09d42fff422
Jeff Fessler <JeffFessler@users.noreply.github.com>
2023-10-02 12:54:29 -0400

    fix

--- docs/make.jl
+++ docs/make.jl
@@ -2,21 +2,23 @@ using Documenter
 using GoogleDrive
 
 makedocs(
-         
-         sitename="GoogleDrive",
-         modules =[GoogleDrive],
-          pages = [
-        "Home" => "index.md",
-        "Features" => "features.md",
-        
+    modules = [GoogleDrive],
+    sitename = "GoogleDrive.jl",
+    pages = [
+     "Home" => "index.md",
+     "Features" => "features.md",
     ],
-    )
-
-deploydocs(repo = "github.com/tejasvaidhyadev/GoogleDrive.jl.git",
-           target = "build",
-           branch = "gh-pages",
-           latest = "mater",
-           osname = "linux"
-    push_preview = true,
 )
 
+deploydocs(
+    repo = "github.com/JuliaIO/GoogleDrive.jl.git",
+    devbranch = "master",
+    target = "build",
+    branch = "gh-pages",
+#   latest = "master",
+#   osname = "linux"
+    devurl = "dev",
+    versions = ["stable" => "v^", "dev" => "dev"],
+    forcepush = true,
+    push_preview = true,
+)
