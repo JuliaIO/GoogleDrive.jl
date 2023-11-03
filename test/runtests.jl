98c89fe339d8e6d8b30392408d8c1c3530954cd5
flexagoon <code@fxgn.dev>
2023-11-03 11:22:23 +0300

    test: expand test coverage

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,13 +1,19 @@
-using GoogleDrive: google_download, drive_download_url
+using GoogleDrive
 using Test
 
 @testset "GoogleDrive.jl" begin
 
-    src = "https://drive.google.com/file/d/XYZ"
     dst = "https://docs.google.com/uc?export=download&id=XYZ"
-    @test drive_download_url(src) == dst
-    @test drive_download_url(dst) == dst
-    @test_throws ArgumentError google_download("foo", IOBuffer())
+    @test google_download_url("https://drive.google.com/file/d/XYZ") == dst
+    @test google_download_url("https://drive.google.com/file/d/XYZ/view") == dst
+    @test google_download_url(dst) == dst
+
+    src = "https://docs.google.com/spreadsheets/d/XYZ/edit"
+    dst = "https://docs.google.com/spreadsheets/d/XYZ/export?format="
+    @test google_download_url(src) == dst * "csv"
+    @test GoogleDrive.sheet_download_url(src, "xlsx") == dst * "xlsx"
+
+    @test_throws ArgumentError google_download_url("foo")
 
     #=
     The following test reads a small Julia file from GoogleDrive.
